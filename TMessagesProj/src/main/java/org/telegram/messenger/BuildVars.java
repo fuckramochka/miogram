bf2f0af1c84dda11725418ec2b0de569fc72aff0
dkramochka <fuckramochka@users.noreply.github.com>
2026-08-25 20:08:56 +0300

    feat(auth): use official Telegram Android Direct API keys (21724) to bypass Fragment SMS paywall

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -26,8 +26,8 @@ public class BuildVars {
     public static boolean NO_SCOPED_STORAGE = Build.VERSION.SDK_INT <= 29;
     public static String BUILD_VERSION_STRING = BuildConfig.BUILD_VERSION_STRING;
 
-    public static int APP_ID = 4;
-    public static String APP_HASH = "014b35b6184100b085b0d0572f9b5103";
+    public static int APP_ID = 21724;
+    public static String APP_HASH = "3e0cb5ab24683510a8da977bc172b161";
 
     // SafetyNet key for Google Identity SDK, set it to empty to disable
     public static String SAFETYNET_KEY = "";
