57bb575252c88e615fa651a97157f174b29fc955
dkramochka <fuckramochka@users.noreply.github.com>
2026-09-13 13:24:21 +0300

    fix(spotify): import ScaleStateListAnimator in MiogramSpotifySheet

--- TMessagesProj/src/main/java/app/miogram/bridge/spotify/MiogramSpotifySheet.java
+++ TMessagesProj/src/main/java/app/miogram/bridge/spotify/MiogramSpotifySheet.java
@@ -23,6 +23,7 @@ import org.telegram.messenger.R;
 import org.telegram.ui.ActionBar.BottomSheet;
 import org.telegram.ui.ActionBar.Theme;
 import org.telegram.ui.Components.LayoutHelper;
+import org.telegram.ui.Components.ScaleStateListAnimator;
 
 import app.miogram.bridge.MiogramLocale;
 import app.miogram.bridge.customui.MiogramHaptic;
